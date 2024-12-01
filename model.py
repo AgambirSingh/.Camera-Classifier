96ca876a35bb5c1ecb889430d6a3f1f61499f3f4
Agambir Singh <sinagamb@sheridancollege.ca>
2024-12-01 18:43:29 -0500

    Train_model function Completed

--- model.py
+++ model.py
@@ -11,3 +11,19 @@ class Model:
     def train_model(self, counters):
         img_list = np.array([])
         class_list = np.array([])
+
+        for i in range(1, counters[0]):
+            img = cv.imread(f'1/frame{i}.jpg')[:, :, 0]
+            img = img.reshape(16800)
+            img_list = np.append(img_list, [img])
+            class_list = np.append(class_list, 1)
+
+        for i in range(1, counters[1]):
+            img = cv.imread(f'2/frame{i}.jpg')[:, :, 0]
+            img = img.reshape(16800)
+            img_list = np.append(img_list, [img])
+            class_list = np.append(class_list, 2)
+
+        img_list = img_list.reshape(counters[0] - 1 + counters[1] - 1, 16800)
+        self.model.fit(img_list, class_list)
+        print("Model successfully trained!")
