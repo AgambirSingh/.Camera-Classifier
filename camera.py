6cf92ca04b93ed5d7f91fee0abd1c62b920587b5
Agambir Singh <sinagamb@sheridancollege.ca>
2024-12-01 18:28:36 -0500

    Final Changes- Adding del and frame function

--- camera.py
+++ camera.py
@@ -9,3 +9,17 @@ class Camera:
 
         self.width = self.camera.get(cv.CAP_PROP_FRAME_WIDTH)
         self.height = self.camera.get(cv.CAP_PROP_FRAME_HEIGHT)
+    def __del__(self):
+        if self.camera.isOpened():
+            self.camera.release()
+
+    def get_frame(self):
+        if self.camera.isOpened():
+            ret, frame = self.camera.read()
+
+            if ret:
+                return (ret, cv.cvtColor(frame, cv.COLOR_BGR2RGB))
+            else:
+                return (ret, None)
+        else:
+            return None
